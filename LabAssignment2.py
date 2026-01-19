cde5d2e3ea20e5381de747ed3081bf857462f8a8
racksyyy <pranavarunkumaraj@gmail.com>
2026-01-19 23:26:23 +0530

    Just saving

--- LabAssignment2.py
+++ LabAssignment2.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np
 
-df_purchase_data=pd.read_excel('Data/Lab Session Data.xlsx', sheet_name='Purchase data')
-print(df_purchase_data)
+def finding_rank_of_feature_matrix():
+    df_purchase_data=pd.read_excel('Data/Lab Session Data.xlsx', sheet_name='Purchase data')
+    clean=df_purchase_data.drop(columns=['Customer']).iloc[:, :4]
+    vt=clean.to_numpy()
+    rank=vt.linalg.matrix_rank()
+    return rank
+
+def cost_using_pseudoinverse():
+    
